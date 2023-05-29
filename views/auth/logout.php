705bc629c7a1650634d6f3bf1f1eb0b5a23ce6c6
imradhe <radhesyam.musicals@gmail.com>
2023-05-30 04:08:19 +0530

    Fixed Logout

--- views/auth/logout.php
+++ views/auth/logout.php
@@ -1,5 +1,9 @@
 <?php
-    controller('Auth');
-    $auth = new Auth();
-    $auth->logout();
+    if(App::getSession()){
+        controller('Auth');
+        $auth = new Auth();
+        $auth->logout();
+    }else{
+        header('Location:'.route(login));
+    }
 ?>
