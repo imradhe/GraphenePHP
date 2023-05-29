3801198f3c3812ab3c9602cebf7c003a4da363c3
imradhe <radhesyam.musicals@gmail.com>
2023-05-30 03:45:38 +0530

    Fixed Images

--- views/errors/401.php
+++ views/errors/401.php
@@ -11,22 +11,22 @@
     <meta name="robots" content="follow"/>
     <link rel="canonical" href="<?php echo url();?>" />
 
-    <link rel=icon sizes=192x192 href="<?php echo $config['APP_ICON']; ?>" />
+    <link rel=icon sizes=192x192 href="<?php echo home().$config['APP_ICON']; ?>" />
     <meta name="theme-color" content="<?php echo $config['APP_THEME_COLOR']; ?>" />
 
      <!-- OPEN GRAPH -->
     <meta property="og:type" content="website" />
     <meta property="og:title" content="<?php echo $config['APP_SHORT_TITLE']; ?>" />
-    <meta property="og:url" content="<?php echo home(); ?>" />
+    <meta property="og:url" content="<?php echo url(); ?>" />
     
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:creator" content="<?php echo $config['APP_TWITTER_CREATOR']; ?>">
 
-    <meta property="og:image:secure_url" itemprop="image" content="<?php echo $config['APP_OG_ICON']; ?>"/>
-    <meta property="og:image" itemprop="image" content="<?php echo $config['APP_OG_ICON']; ?>"/>
+    <meta property="og:image:secure_url" itemprop="image" content="<?php echo home().$config['APP_OG_ICON']; ?>"/>
+    <meta property="og:image" itemprop="image" content="<?php echo home().$config['APP_OG_ICON']; ?>"/>
 
-    <meta property="og:image:secure_url" itemprop="image" content="<?php echo $config['APP_OG_ICON_MOBILE']; ?>"/>
-    <meta property="og:image" itemprop="image" content="<?php echo $config['APP_OG_ICON_MOBILE']; ?>"/>
+    <meta property="og:image:secure_url" itemprop="image" content="<?php echo home().$config['APP_OG_ICON_MOBILE']; ?>"/>
+    <meta property="og:image" itemprop="image" content="<?php echo home().$config['APP_OG_ICON_MOBILE']; ?>"/>
     <meta property="og:description" content="<?php echo $config['APP_DESC']; ?>" />
     <meta property="og:site_name" content="<?php echo $config['APP_NAME']; ?>"/>
     
