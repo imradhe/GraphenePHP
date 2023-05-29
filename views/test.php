a71cc7baed06359654f8c5f346c682bfd57f628a
imradhe <radhesyam.musicals@gmail.com>
2023-05-30 05:08:07 +0530

    Tested Validator Class

--- views/test.php
+++ views/test.php
@@ -1,17 +1,39 @@
 <?php
-errors(1);
-controller('Auth');
-$auth = new Auth;
-
-$data = [
-    'name' => 'Arun Devaputra',
-    'email' => 'arun@graphenephp.com',
-    'phone' => '8839929302',
-    'password' => 'Radhe@M022',
-    'role' => 'user'
+require('models/validator.php');
+$name = "Radhe Shyam";
+$email = "arun@abc.com";
+$fields = [
+    'name' => [
+        'value' => $name,
+        'rules' => [
+            [
+                'type' => 'required',
+                'message' => "Name can't be empty",
+            ],
+            [
+                'type' => 'minLength',
+                'message' => "Name can't be less than 6 characters",
+                'minLength' => 6,
+            ]
+        ]
+    ],
+    'email' => [
+        'value' => $email,
+        'rules' => [
+            [
+                'type' => 'required',
+                'message' => "Email can't be empty",
+            ],
+            [
+                'type' => 'email',
+                'message' => 'Email is invalid',
+            ],
+        ]
+    ]
 ];
-//$name, $email, $phone, $password, $role
-$result = $auth->delete($data['email']);
 
+// Call the validateFields function
+$result = Validator::validate($fields);
+
+// Access the validation result
 echo json_encode($result);
-
