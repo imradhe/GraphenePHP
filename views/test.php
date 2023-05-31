4b21fce4f969032069a7f51d7b1a73dfa186e325
imradhe <radhesyam.musicals@gmail.com>
2023-05-31 12:36:50 +0530

    Tested Migrator

--- views/test.php
+++ views/test.php
@@ -1,17 +1,14 @@
 <?php
-//errors(1);
-controller('Auth');
-$auth = new Auth;
 
-$data = [
-    'name' => 'Arun Devaputra',
-    'email' => 'contact@imradheaaaa.com',
-    'phone' => '8839929302',
-    'password' => 'Radhe@M022',
-    'role' => 'user'
+// Define the SQL table names and their corresponding SQL files
+$tables = [
+    'users' => 'auth/users.sql',
+    'logs' => 'auth/logs.sql',
+    // Add more tables as needed
 ];
-//$name, $email, $phone, $password, $role
-$result = $auth->register($data['name'], $data['email'], $data['phone'], $data['password'], $data['role']);
 
+// Migrate tables
+$migrate = Migrator::migrate($tables);
 
-echo json_encode($result);
+echo json_encode($migrate);
+
