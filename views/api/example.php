ebdbd2bcd1e86d12efceb00576bb7075976ea271
imradhe <radhesyam.musicals@gmail.com>
2023-05-31 11:47:16 +0530

    Formatted

--- views/api/example.php
+++ views/api/example.php
@@ -1,17 +1,17 @@
-<?php    
+<?php
 $url = 'https://jsonplaceholder.typicode.com/posts';
 $data = json_decode(file_get_contents($url));
 
-if(!empty($data)){
+if (!empty($data)) {
     $res['status'] = '200';
     $res['message'] = 'Success';
     $res['data'] = $data;
-}else{
+} else {
     $res['status'] = '500';
     $res['message'] = "No Data";
     $res['data'] = $data;
 }
 
 http_response_code($res['status']);
-header('Content-Type: application/json');    
+header('Content-Type: application/json');
 echo json_encode($res);
