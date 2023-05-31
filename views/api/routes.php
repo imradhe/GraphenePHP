75c0c35635b467fc04423cd61f9807d451434761
imradhe <radhesyam.musicals@gmail.com>
2023-05-31 11:47:25 +0530

    Formatted

--- views/api/routes.php
+++ views/api/routes.php
@@ -1,18 +1,18 @@
-<?php    
-$data = json_decode(file_get_contents('routes.json')); 
+<?php
+$data = json_decode(file_get_contents('routes.json'));
 
 
 
-if(!empty($data)){
+if (!empty($data)) {
     $res['status'] = '200';
     $res['message'] = 'Success';
     $res['data'] = $data;
-}else{
+} else {
     $res['status'] = '500';
     $res['message'] = 'No Routes';
     $res['data'] = $data;
 }
 
 http_response_code($res['status']);
-header('Content-Type: application/json');    
+header('Content-Type: application/json');
 echo json_encode(array_reverse($res));
