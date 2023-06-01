70873d52e2e7d783b7b3cb31f45b4b3e0a7c4748
imradhe <radhesyam.musicals@gmail.com>
2023-06-01 17:47:26 +0530

    Fixed App Controller

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -1,39 +1,48 @@
 <?php
+
+/**
+ * The App class provides utility methods for the main application.
+ */
 class App
 {
-
-
-
-    // Get Current Session
+    /**
+     * Retrieves the current session information.
+     *
+     * @return array|false The session information if available, or false otherwise.
+     */
     public static function getSession()
     {
         $loginID = $_COOKIE['auth'];
         DB::connect();
         $query = DB::select('logs', '*', "loginID='$loginID' and loggedout=0")->fetchAll();
         DB::close();
+        
         if ($query) {
             return $query[0];
-        } else
+        } else {
             return false;
+        }
     }
 
-
-    // Get User from cookie
+    /**
+     * Retrieves the user information from the cookie.
+     *
+     * @return array|false The user information if available, or false otherwise.
+     */
     public static function getUser()
     {
         $loginID = $_COOKIE['auth'];
 
         DB::connect();
         $query = DB::select('logs', '*', "loginID='$loginID' and loggedout=0")->fetchAll()[0];
+        
         if ($query) {
             $email = $query['email'];
             $currentLog = DB::select('users', '*', "email='$email'")->fetchAll();
             DB::close();
             return $currentLog[0];
-        } else
+        } else {
             return false;
+        }
     }
-
-
-
 }
