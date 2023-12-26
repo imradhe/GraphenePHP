0789650761ce1a2a7da9221180940546069cb199
imradhe <radhesyam.musicals@gmail.com>
2023-12-26 09:49:49 +0530

    Modified models

--- models/auth/logs.sql
+++ models/auth/logs.sql
@@ -1,20 +1,9 @@
 CREATE TABLE logs (
-    loginID varchar(32) NOT NULL,
-    email varchar(30) NOT NULL,
-    loggedinat TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    ip varchar(255) NOT NULL,
-    os varchar(255) NOT NULL,
-    browser varchar(255) NOT NULL,
-    loggedout int DEFAULT 0,
-    loggedoutat TIMESTAMP NULL,
-    FOREIGN KEY (email) REFERENCES users(email),
-    PRIMARY KEY (loginID)
+    loginId VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255),
+    loggedInAt DATETIME,
+    ip VARCHAR(255),
+    os VARCHAR(255),
+    browser VARCHAR(255),
+    loggedOutAt DATETIME
 );
-INSERT INTO users (`name`, `email`, `password`, `phone`, `role`)
-VALUES (
-        "Arun",
-        "arun@graphenephp.com",
-        "900150983cd24fb0d6963f7d28e17f72",
-        "9121325466",
-        "admin"
-    )
