9ba69e62ce1d1b21bece278e087464177cfa12ce
imradhe <radhesyam.musicals@gmail.com>
2023-12-26 10:27:23 +0530

    Modified setup

--- setup.php
+++ setup.php
@@ -6,97 +6,116 @@ error_reporting(1);
 
 require('models/migrator.php');
 require('models/db.php');
+
 // Task 2: Display form to update database details
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     ?>
+    <!doctype html>
+    <html lang="en">
 
-<!doctype html>
-<html lang="en">
-<head>
+    <head>
 
-    <!-- Bootstrap core CSS -->
-    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- Bootstrap core CSS -->
+        <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- Icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+        <!-- Jquery -->
+        <script src="assets/jquery/jquery.min.js"></script>
 
-    <!-- Custom styles for this template -->
-    <link rel="stylesheet" href="assets/css/app.css">
-<style>
-  .form-setup {
-    width: 100%;
-    max-width: 330px;
-    padding: 15px;
-    margin-top: 2vh !important;
-    margin: auto;
-  }
+        <!-- Custom styles for this template -->
+        <link rel="stylesheet" href="assets/css/app.css">
+        <style>
+            .form-setup {
+                width: 100%;
+                max-width: 330px;
+                padding: 15px;
+                margin-top: 2vh !important;
+                margin: auto;
+            }
 
-  .form-setup .checkbox {
-    font-weight: 400;
-  }
+            .form-setup .checkbox {
+                font-weight: 400;
+            }
 
-  .form-setup .form-control {
-    position: relative;
-    box-sizing: border-box;
-    height: auto;
-    padding: 10px;
-    font-size: 16px;
-  }
+            .form-setup .form-control {
+                position: relative;
+                box-sizing: border-box;
+                height: auto;
+                padding: 10px;
+                font-size: 16px;
+            }
 
-  .form-setup .form-control:focus {
-    z-index: 2;
-  }
+            .form-setup .form-control:focus {
+                z-index: 2;
+            }
 
-  .form-setup input[type="email"] {
-    margin-bottom: -1px;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-  }
+            .form-setup input[type="email"] {
+                margin-bottom: -1px;
+                border-bottom-right-radius: 0;
+                border-bottom-left-radius: 0;
+            }
 
-  .form-setup input[type="password"] {
-    margin-bottom: 10px;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-  }
+            .form-setup input[type="password"] {
+                margin-bottom: 10px;
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+            }
 
-  #eye {
-    cursor: pointer;
-  }
+            #eye {
+                cursor: pointer;
+            }
 
-  .logo img {
-    max-height: 12vh;
-  }
-</style>
-</head>
-<body class="text-center mt-5">
-    <h2>GraphenePHP App Setup</h2>
-    <form method="post" class="form-setup">
-        <?php
+            .logo img {
+                max-height: 12vh;
+            }
+        </style>
+    </head>
 
-        // Assuming you have a function to read the existing config.php
-        require('config_example.php');
-        ?>
-        <div class="mb-3">
-        <label for="db_host" class="form-label">Database Host</label>
-        <input type="text" name="db_host" class="form-control" value="<?= $config['DB_HOST'] ?>" required>
-    </div>
+    <body class="text-center mt-5">
+        <h2>GraphenePHP App Setup</h2>
+        <form method="post" class="form-setup">
+            <?php
 
-    <div class="mb-3">
-        <label for="db_name" class="form-label">Database Name</label>
-        <input type="text" name="db_name" class="form-control" value="<?= $config['DB_DATABASE'] ?>" required>
-    </div>
+            // Assuming you have a function to read the existing config.php
+            require('config_example.php');
+            ?>
+            <div class="mb-3">
+                <label for="app_slug" class="form-label">App Slug </label>
+                <input type="text" name="app_slug" class="form-control" value="<?= $config['APP_SLUG'] ?>">
+                
+                <strong>What is slug? <i class="bi bi-info-circle fs-5 text-primary" type="button" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-custom-class="custom-tooltip" data-bs-html="true" data-bs-title="Slug is the name of your directory.<br>For example if your directory name is <b>'MyCoolApp'</b>, it is your slug<br> <br> Note: If you are hosting it in a server and the app is in root directory leave this empty"></i>
+</strong>
+            </div>
 
-    <div class="mb-3">
-        <label for="db_user" class="form-label">Database User</label>
-        <input type="text" name="db_user" class="form-control" value="<?= $config['DB_USERNAME'] ?>" required>
-    </div>
+            <div class="mb-3">
+                <label for="db_host" class="form-label">Database Host</label>
+                <input type="text" name="db_host" class="form-control" value="<?= $config['DB_HOST'] ?>" required>
+            </div>
 
-    <div class="mb-3">
-        <label for="db_password" class="form-label">Database Password</label>
-        <input type="password" name="db_password" class="form-control" value="<?= $config['DB_PASSWORD'] ?>">
-    </div>
+            <div class="mb-3">
+                <label for="db_name" class="form-label">Database Name</label>
+                <input type="text" name="db_name" class="form-control" value="<?= $config['DB_DATABASE'] ?>" required>
+            </div>
 
-    <button type="submit" class="btn btn-graphene">Update</button>
-    </form>
+            <div class="mb-3">
+                <label for="db_user" class="form-label">Database User</label>
+                <input type="text" name="db_user" class="form-control" value="<?= $config['DB_USERNAME'] ?>" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="db_password" class="form-label">Database Password</label>
+                <input type="password" name="db_password" class="form-control" value="<?= $config['DB_PASSWORD'] ?>">
+            </div>
+
+            <button type="submit" class="btn btn-graphene">Finish</button>
+        </form>
+        <script>
+            const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+            const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+        </script>
     </body>
+
     </html>
 
     <?php
@@ -109,21 +128,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Task 4: Copy config_example.php after updating database details
     if (copy('config_example.php', 'config.php')) {
-        echo 'config.php updated successfully. config_example.php copied.';
-        
+
         // Task 5: Remove config_example.php
         unlink('config_example.php');
-        
+
         // Task 6: Create a new database
         createDatabase($_POST['db_name']);
     } else {
         echo 'Error copying config_example.php';
     }
-
 }
 
 // Function to update database details in config.php
-function updateDatabaseDetails($data) {
+function updateDatabaseDetails($data)
+{
     // Include the config file with $config as an array
     require('config_example.php');
 
@@ -132,6 +150,7 @@ function updateDatabaseDetails($data) {
     $config['DB_DATABASE'] = $data['db_name'];
     $config['DB_USERNAME'] = $data['db_user'];
     $config['DB_PASSWORD'] = $data['db_password'];
+    $config['APP_SLUG'] = $data['app_slug'];
 
     // Write updated config.php
     $content = "<?php\n\n";
@@ -149,7 +168,7 @@ function updateDatabaseDetails($data) {
     $content .= '$config = [' . PHP_EOL;
 
     foreach ($config as $key => $value) {
-        if (in_array($key, ['DB_HOST', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD'])) {
+        if (in_array($key, ['DB_HOST', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD', 'APP_SLUG'])) {
             $content .= sprintf("    '%s' => '%s',\n", $key, addslashes($config[$key]));
         } else {
             $content .= sprintf("    '%s' => %s,\n", $key, var_export($config[$key], true));
@@ -164,7 +183,8 @@ function updateDatabaseDetails($data) {
 }
 
 // Function to create a new database
-function createDatabase($dbName) {
+function createDatabase($dbName)
+{
     require('config.php');
     // Assuming you have a function to establish a database connection
     $mysqli = new mysqli($config['DB_HOST'], $config['DB_USERNAME'], $config['DB_PASSWORD']);
@@ -177,18 +197,18 @@ function createDatabase($dbName) {
     // Create database
     $createDbQuery = "CREATE DATABASE IF NOT EXISTS $dbName";
     if ($mysqli->query($createDbQuery) === TRUE) {
-        echo "Database $dbName created successfully";
-        
+
         $tables = [
             'users' => 'auth/users.sql',
             'logs' => 'auth/logs.sql',
             // Add more tables as needed
         ];
-        
+
         // Migrate tables
         $migrate = Migrator::migrate($tables);
-        
-        echo json_encode($migrate);
+
+
+
         unset($_REQUEST);
         ?>
         <script>
