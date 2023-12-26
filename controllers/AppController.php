ec3d97887d80c0880c37bc41ab5d15b94300a208
imradhe <radhesyam.musicals@gmail.com>
2023-12-26 10:38:48 +0530

    Updated version to 2.0.0

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -2,6 +2,15 @@
 
 /**
  * The App class provides utility methods for the main application.
+ *
+ * GraphenePHP App Controller
+ *
+ * This class provides validation functionalities for form fields.
+ * It allows defining validation rules and callbacks for each field,
+ * and returns error messages for invalid fields.
+ *
+ * @package GraphenePHP
+ * @version 2.0.0
  */
 class App
 {
