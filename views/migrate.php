008f4519d05084387828b2515b7ea48b62760326
imradhe <radhesyam.musicals@gmail.com>
2023-12-26 09:50:06 +0530

    Fixed

--- views/migrate.php
+++ views/migrate.php
@@ -1,14 +1,12 @@
 <?php
-
 // Define the SQL table names and their corresponding SQL files
-$tables = [
-    'users' => 'auth/users.sql',
-    'logs' => 'auth/logs.sql',
-    // Add more tables as needed
-];
-
-// Migrate tables
-$migrate = Migrator::migrate($tables);
-
-echo json_encode($migrate);
-
+        $tables = [
+            'users' => 'auth/users.sql',
+            'logs' => 'auth/logs.sql',
+            // Add more tables as needed
+        ];
+        
+        // Migrate tables
+        $migrate = Migrator::migrate($tables);
+        
+        echo json_encode($migrate);
