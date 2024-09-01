46296b28627eecb80fadd7e485e1d87137fa0e97
imradhe <radhesyam.musicals@gmail.com>
2024-09-01 15:54:05 +0530

    Added a comment for clarity

--- views/migrate.php
+++ views/migrate.php
@@ -1,5 +1,8 @@
 <?php
 // Define the SQL table names and their corresponding SQL files
+
+// Make sure you define tables in the right order for foreign key constraints
+
         $tables = [
             'users' => 'auth/users.sql',
             'logs' => 'auth/logs.sql',
